0c1f03033df71248504b5e1841f5f59cfff1ef7c
Glitch (pool-ringer) <none>
2019-02-27 21:41:29 +0000

    🎒🏬 Checkpoint ./myapp.js:2398890/241

--- myapp.js
+++ myapp.js
@@ -3,7 +3,9 @@ mongoose.connect(process.env.MONGO_URI);
 
 //Create Url Model 
 
-var urlSchema = new mongooose.Schema({
-  url: {type: String, 
-  
-})
+var urlSchema = new mongoose.Schema({
+  original_url: {type: String, required: true},
+  short_url: {unique: true, index: true}
+});
+
+var Url = mongoose.model('Url', urlSchema)
