d0e81ef1f158e878cc7fa41f45cf121b03c4d37c
Glitch (pool-ringer) <none>
2019-02-28 00:09:16 +0000

    🛀💪 Checkpoint ./url.js:2398890/302 ./server.js:2398890/169

--- server.js
+++ server.js
@@ -19,17 +19,13 @@ app.use(cors());
 /** this project needs to parse POST bodies **/
 // you should mount the body-parser here
 
+var Url = require("./url.js").UrlModel;
 app.use('/public', express.static(process.cwd() + '/public'));
 
 app.get('/', function(req, res){
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
-  
-// your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
-});
 
 var createUrl = require("./url.js").createAndSaveURL;
 app.get("/api/shorturl/new", function (req, res) {
--- url.js
+++ url.js
@@ -13,10 +13,19 @@ var Url = mongoose.model('Url', urlSchema);
 //create and save url 
 var createAndSaveURL = function(long_url, done) {
     //check to see if this url hasn't already been entered.
+  var url = new Url ({long_url: url, short_url: 
   
 };
 
+// var createAndSavePerson = function(done) {
+//   var joe = new Person({ name: 'joe', age: '22'});
+//   joe.save((err, data) => {
+//     if (err) console.log(err);
+//     return done(null ,data);
+//   });
+// };
+
 //get url
 var getUrlByShortUrl = function(short_url, done) {
-    Url.findOne({short_url: short_url}, function(err, url) {} )
+    Url.findOne({short_url: short_url}, function(err, url) {});
 }
