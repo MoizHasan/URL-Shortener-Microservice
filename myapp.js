e6a9446f18735ea67bb6725201536abd67cedf3b
Glitch (pool-ringer) <none>
2019-03-02 07:06:32 +0000

    🚨🎨 Checkpoint ./server.js:2398890/228 ./url.js:2398890/4 ./myapp.js:2398890/1579

--- myapp.js
+++ myapp.js
@@ -21,25 +21,10 @@ app.use(cors());
 const body_parser = require('body-parser');
 app.use(body_parser.json());
 app.use(body_parser.urlencoded({extended: false}));
+app.use('/urls', 
 
 const product_controller = require('../controllers/product.controller');
 
-var Url = require("./url.js").UrlModel;
-app.use('/public', express.static(process.cwd() + '/public'));
-
-app.get('/', function(req, res){
-  res.sendFile(process.cwd() + '/views/index.html');
-});
-
-
-var createUrl = require("./url.js").createAndSaveUrl;
-app.post("/api/shorturl/new", function (req, res) {
-    
-});
-
-app.get("./api/shorturl/:short_url", function (req, res) {
-  res.redirect(req.params.long_url);
-});
 
 app.listen(port, function () {
   console.log('Node.js listening ...');
